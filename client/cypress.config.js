4bab06cb04a091704a0d4b52069b28fdc585ac55
slagerprog <aslager@gmail.com>
2025-10-12 10:53:54 -0400

    NEA-131 Add'l config

--- client/cypress.config.js
+++ client/cypress.config.js
@@ -3,6 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
+    video: false,
+    screenshotOnRunFailure: false,
     setupNodeEvents(on, config) {
       // add code coverage task
       require("@cypress/code-coverage/task")(on, config);
