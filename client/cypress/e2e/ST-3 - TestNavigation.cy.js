ad5d9e8cf4977b2b78509b39630b888720d9b84d
slagerprog <aslager@gmail.com>
2025-10-22 09:56:26 -0400

    Configured ST-3 to Remove Test for Performance Metrics Placeholder

--- client/cypress/e2e/ST-3 - TestNavigation.cy.js
+++ client/cypress/e2e/ST-3 - TestNavigation.cy.js
@@ -55,9 +55,6 @@ it("Displays the correct navigation pages while navigating through the System", 
     '#root [href="/transactions/collateralpledgetransactions/collateralpledgeremove"]',
   ).click();
   cy.contains("Placeholder for Collateral Pledge Remove").should("be.visible");
-  cy.get('#root [href="/performance/"]').click();
-  cy.get('#root [href="/performance/updatemetrics"]').click();
-  cy.contains("Placeholder for Update Metrics").should("be.visible");
   cy.get('#root [href="/reporting/"]').click();
   cy.contains("Placeholder for Reporting").should("be.visible");
   cy.get('#root [href="/dashboard/"]').click();
