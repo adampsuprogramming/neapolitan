93f62212ba22dd0a4014ab39080ba70358d58166
slagerprog <aslager@gmail.com>
2025-11-26 15:28:10 -0500

    Updated landing page and fixed Cypress Tests

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,19 +5,50 @@ import { useAuth0 } from "@auth0/auth0-react";
 const Home = () => {
   const { user, isAuthenticated } = useAuth0();
   return (
-    <div className="home">
-      <h1>Welcome to Neapolitan Debt Management Software</h1>
+    <div className="home" style={{ padding: '0', margin: '0'}}>
+      <h1 style ={{
+        fontSize: '96px',
+        marginBottom: '0px',
+        paddingBottom: '0px',
+        paddingLeft: '40px',
+
+      }}>Welcome to</h1>
+      <h1 style ={{
+        fontSize: '96px',
+        marginTop: '0px',
+        paddingTop: '0px',
+        marginBottom: '0px',
+        paddingBottom: '0px',
+        paddingLeft: '40px',
+      }}>neapolitan.</h1>
+      
+      <h2 style ={{
+        fontSize: '40px',
+        marginBottom: '0px',
+        paddingBottom: '0px',
+        paddingLeft: '40px',
+        }}>
+      <span className="arrow" style={{ color: '#F26419'}}>&gt;</span> simplify your asset-based financing</h2>
       {isAuthenticated && user ? (
         <>
-          <h3>
-            Please click on <LogoutButton /> if you wish to log out of the system.{" "}
-          </h3>
+          <h2 style ={{
+        fontSize: '24px',
+        marginBottom: '0px',
+        paddingBottom: '0px',
+        paddingLeft: '40px',
+        marginTop: '7ch'}}>
+            Click on <LogoutButton /> to safely log out.{" "}
+          </h2>
         </>
       ) : (
-        <h3>
-          Please click on <LoginButton /> to log into Neapolitan or contact your adminstrator for
-          access{" "}
-        </h3>
+        <h2 style ={{
+        fontSize: '24px',
+        marginBottom: '0px',
+        paddingBottom: '0px',
+        paddingLeft: '40px',
+        marginTop: '7ch'}}>
+          Please <LoginButton /> to continue{" "}
+        </h2>
       )}
       <br></br>
     </div>
